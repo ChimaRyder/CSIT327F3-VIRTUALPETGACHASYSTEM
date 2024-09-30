e3519c75e7fa79391917240ab9677ebd4c1a7e4b
frenzr <frenzrpnte@gmail.com>
2024-09-30 23:41:40 +0800

    added views for inventory_list and pet_view.
    As a result, a pet model has been created. This may be changed for future development.
    
    Furthermore, corresponding html files have been made for the templates, but still need integration with the base template and the backend. This will be next on the agenda.

--- virtualgacha/inventory/models.py
+++ virtualgacha/inventory/models.py
@@ -5,3 +5,16 @@ from django.contrib.auth.models import User # Can be changed later if a custom U
 class Inventory(models.Model):
     total_pets = models.IntegerField(default=0)  # Needs pet keys to change to a derived field
     owner_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Pets(models.Model):
+    class Rarity(models.IntegerChoices):
+        COMMON = 0
+        UNCOMMON = 1
+        RARE = 2
+        MYTHICAL = 3
+        LEGENDARY = 4
+
+    pet_species = models.CharField(max_length=50)
+    rarity = models.IntegerField(choices=Rarity.choices)
+    is_busy = models.BooleanField(default=False)
+    inventory = models.ForeignKey(Inventory, on_delete=models.CASCADE)
--- virtualgacha/inventory/views.py
+++ virtualgacha/inventory/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
 
+from inventory.models import Inventory, Pets
+
+
 # inventory list backend
+def inventory_list(request):
+    user = request.user
+
+    inventory = Inventory.objects.get(owner_id=user.id)
+
+    #find pets associated with inventory
+    pets = Pets.objects.filter(inventory_id=inventory.id)
+
+    return render(request, "inventory/inventory_list.html", {'inventory': inventory, 'pets': pets})
+
 
 # pet view in inventory
+def pet_view(request):
+    pet_id = request.GET.get('pet_id')
+    pet = Pets.objects.get(id=pet_id)
+
+    return render(request, "pets/pet_view.html", {'pet': pet})
+
 
 # send to an adventure (collect credits?)
-
-
+def send_adventure(request):
+    pass
