522badff2f27b3610ddfdf7b9a4fe802ee5e6614
Rara <107735270+rayalindley@users.noreply.github.com>
2024-10-03 06:34:12 +0800

    added fields auth

--- virtualgacha/login_register/admin.py
+++ virtualgacha/login_register/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
+from .models import Profile
 
-# Register your models here.
 class UserAdmin(admin.ModelAdmin):
     list_display = ('username', 'email', 'is_staff', 'is_active')
 
 admin.site.unregister(User)
-admin.site.register(User, UserAdmin) 
+admin.site.register(User, UserAdmin)
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'first_name', 'last_name', 'birthdate')
+
+admin.site.register(Profile, ProfileAdmin)
