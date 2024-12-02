8fc41089bcd9278a9761c077de92c842084e0ade
Rara <107735270+rayalindley@users.noreply.github.com>
2024-12-02 22:23:55 +0800

    found it

--- virtualgacha/chat/views.py
+++ virtualgacha/chat/views.py
@@ -11,13 +11,19 @@ def message_list(request):
         room = ChatRoom.objects.get(name='Global Chat')
         messages = room.messages.all().order_by('timestamp')
         message_list = []
+
         for message in messages:
+            sender_profile = message.sender.profile
+
             message_list.append({
                 'sender': message.sender.username,
                 'content': message.content,
                 #'timestamp': message.timestamp.strftime('%Y-%m-%d %H:%M')
-                'timestamp': message.timestamp.strftime('%I:%M %p')
+                'timestamp': message.timestamp.strftime('%I:%M %p'),
+                'sender_profile_image_url': sender_profile.get_profile_image_url()
             })
+
         return JsonResponse(message_list, safe=False)
+    
     except ChatRoom.DoesNotExist:
         return JsonResponse({'error': 'Chat room not found.'}, status=404)
