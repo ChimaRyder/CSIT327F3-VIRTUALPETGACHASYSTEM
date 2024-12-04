dd7e2323c25275ab381f9799e7b11b33c62c3aca
tr-ggr <tristanjamestolentino56@gmail.com>
2024-12-04 08:41:27 +0800

    Populate db fixes

--- virtualgacha/scripts/create_trades.py
+++ virtualgacha/scripts/create_trades.py
@@ -4,13 +4,23 @@ import random
 from django.utils import timezone
 from inventory.models import Inventory
 from trading.models import Trade
+from datetime import timedelta
 
 # Initialize Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project.settings')
 django.setup()
 
+def random_date(start, end):
+    return start + timedelta(
+        seconds=random.randint(0, int((end - start).total_seconds())),
+    )
+
+
 def create_dummy_trades(num_trades=10):
     inventories = list(Inventory.objects.all())
+
+    end_date = timezone.now()
+    start_date = end_date - timedelta(days=365) 
 
     if len(inventories) < 2:
         print("Not enough inventory items to create trades.")
@@ -22,13 +32,16 @@ def create_dummy_trades(num_trades=10):
         while pet_to_offer == pet_to_trade:
             pet_to_offer = random.choice(inventories)
 
+        date_created = random_date(start_date, end_date)
+
         trade = Trade.objects.create(
             pet_to_trade=pet_to_trade,
-            pet_to_offer=pet_to_offer,
-            pet_preferences=[random.choice(inventories).id for _ in range(random.randint(1, 5))],
-            status=random.choice([status for status, _ in Trade.TradeStatus.choices]),
-            date_created=timezone.now(),
-            date_completed=timezone.now() if random.choice([True, False]) else None
+            pet_to_offer=pet_to_offer if random.choice([True, False]) else None,
+            pet_preferences = [
+                f"{pet.pet_id.get_rarity_display()}/{pet.pet_id.pet_species}"
+                for pet in random.sample(inventories, random.randint(1, 4))
+            ],
+            date_created=date_created,
         )
         print(f"Created trade: {trade}")
 
